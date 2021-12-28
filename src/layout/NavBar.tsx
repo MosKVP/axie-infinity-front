aa3ea2610d26942758b2a70d07e96f3e96a77660
Kavinpon <kavinpon@hotmail.com>
2021-12-29 01:43:17 +0700

    style: change icon

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -1,12 +1,15 @@
 import { Stack } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
 
 const NavBar: React.FC = () => {
   return (
     <nav className='navigation'>
       <ul className='navigation__inner'>
         <div>
-          <Link to='/'>Axie</Link>
+          <Link to='/'>
+            <img src={Logo} alt='logo' className="navigation__logo"></img>
+          </Link>
         </div>
         <Stack direction='row'>
           <li>
