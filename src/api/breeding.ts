ddbe28c4aa842727e650305532dd49ab564a6e7d
Kavinpon <kavinpon@hotmail.com>
2022-01-09 01:09:06 +0700

    refactor: get backend url from .env

--- src/api/breeding.ts
+++ src/api/breeding.ts
@@ -1,7 +1,8 @@
 import { CalculateRequest, CalculateResult } from "./breeding.d";
 import { httpClient } from "../middlewares/httpClient";
 
-const URL = "/api/v1/breeding/calculate";
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const URL = BASE_URL + "/api/v1/breeding/calculate";
 
 export const calculate = async (payload: CalculateRequest) =>
   httpClient.post<CalculateResult>(URL, payload);
